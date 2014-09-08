5e429af4fde3093d7d97ca5b3a73811890ccf06d
Tareq Hassan <tareqhassan54@gmail.com>
2014-09-08 14:53:32 +0300

    Updating New Features

--- TempCart.js
+++ TempCart.js
@@ -11,6 +11,9 @@ TempCart = {
 		}
 		else{
 			self.renderTemplate(products,options.listingHtmlTemplate,options.appendListingTo,options.currency);
+			if(self.checkLocalStorageCart()){
+				self.getLocalStorageCart();
+			}
 			if(typeof options.renderProductsFallBack == "function"){
 				options.renderProductsFallBack();
 			}
@@ -29,36 +32,48 @@ TempCart = {
 	updateCart:function(id,type){
 		var self = this;
 		var options = self.options;
-		if(typeof JsonCart == "undefined"){
-			JsonCart = new Array;
+		if(typeof self.JsonCart == "undefined"){
+			self.JsonCart = new Array;
 		}
-		if(self.getProductbyProperty(JsonCart,'id',id)){
-			var cartProduct = self.getProductbyProperty(JsonCart,'id',id);
+		if(self.getProductbyProperty(self.JsonCart,'id',id)){
+			var cartProduct = self.getProductbyProperty(self.JsonCart,'id',id);
 			if(type == "add"){
 				cartProduct.qty++;
 				options.addProductFallBack();
-			}
+			} 
 			else{
 				cartProduct.qty--;
-				self.deleteObjectId(id,JsonCart);
+				self.deleteObjectId(id,self.JsonCart);
 				options.removeProductFallBack();
 			}
 		}else{
 			product = self.getProductbyProperty(options.products,'id',id);
 			product.qty = 1;
-			JsonCart.push(product);
+			self.JsonCart.push(product);
 			options.addProductFallBack();
 		}
 		var cartTemplate = options.cartHtmlTemplate;
 		var cartHtmlTarget = options.cartHtmlObject;
 		var currency = options.currency;
-		self.renderTemplate(JsonCart,cartTemplate,cartHtmlTarget,currency);
+		self.JsonCart['total'] = self.getCartTotal(self.JsonCart);
+		if(options.saveCart){
+			self.pushLocalStorageCart(self.JsonCart);
+		}
+		self.renderTemplate(self.JsonCart,cartTemplate,cartHtmlTarget,currency);
+	},
+	getCartTotal:function(cart){
+		var total = 0;
+		for (var i = cart.length - 1; i >= 0; i--) {
+			currentProduct = cart[i];
+			total += currentProduct.price * currentProduct.qty;
+		};
+		return total;
 	},
 	validateProductsJson:function(products){
 		var self = this;
 		var validateThese = ['name','id','price','image'];
 		if(products.length > 0){
-			for(i in products){
+			for(var i in products){
 				for (var x = 0; x < validateThese.length; x++) {
 					if(!products[i].hasOwnProperty(validateThese[x])){
 						self.consoleLog('warn',"product #"+ i +" doesn't have "+validateThese[x]);
@@ -71,6 +86,28 @@ TempCart = {
 		}else{
 			return false;
 		}
+	},
+	pushLocalStorageCart:function(cart){
+		if(typeof(Storage)!=="undefined"){
+			var cartString = JSON.stringify(cart);
+			localStorage.setItem("TempCart", cartString);
+		  }	
+	},
+	getLocalStorageCart:function(){
+		var self = this;
+		var options = self.options;
+		if(typeof(Storage)!=="undefined"){
+			var cartString = localStorage.getItem('TempCart');
+			self.JsonCart = JSON.parse(cartString);
+			var cartTemplate = options.cartHtmlTemplate;
+			var cartHtmlTarget = options.cartHtmlObject;
+			var currency = options.currency;
+			self.JsonCart['total'] = self.getCartTotal(self.JsonCart);
+			self.renderTemplate(self.JsonCart,cartTemplate,cartHtmlTarget,currency);
+		  }	
+	},
+	checkLocalStorageCart:function(){
+		return typeof(Storage) !=="undefined" && localStorage.getItem('TempCart').length > 0;	
 	},
 	consoleLog:function(type,message){
 		if(self.consoleReady == true){
