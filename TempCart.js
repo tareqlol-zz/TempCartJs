dca2c00223a7c08d6ff7c9397a90a722204efce5
Tareq Hassan <tareqhassan54@gmail.com>
2014-09-11 13:17:29 +0300

    updating the main script

--- TempCart.js
+++ TempCart.js
@@ -1,134 +1,132 @@
-TempCart = {
-	init:function(tempCartOptions){
-		var self = this;
-		self.options = tempCartOptions;
-		var options = self.options;
-		var products = options.products;
-		typeof console == "object" ? self.consoleReady = true : self.consoleReady == false;
-			
-		if(!self.validateProductsJson(products)){
-			self.consoleLog('error','Please revise the products JSON object');
-		}
-		else{
+TempCart =  new function(){
+
+	var options,
+		products,
+		self = this,
+		consoleReady = typeof console == "object",
+		JsonCart = [];
+
+	var cartActions = {
+			add:function(cartProduct){
+				cartProduct.qty++;
+				if(typeof options.addProductCallBack == "function"){
+					options.addProductCallBack();
+				}
+			},
+			remove:function(cartProduct){
+				cartProduct.qty--;
+				self.deleteObjectId(cartProduct.id,JsonCart);
+				if(typeof options.removeProductCallBack == "function"){
+					options.removeProductCallBack();
+				}
+			}
+	};
+
+	this.init = function(tempCartOptions){
+		options = tempCartOptions;
+		products = options.products;
+		if(self.validateProductsJson(products)){
 			self.renderTemplate(products,options.listingHtmlTemplate,options.appendListingTo,options.currency);
 			if(self.checkLocalStorageCart()){
 				self.getLocalStorageCart();
 			}
-			if(typeof options.renderProductsFallBack == "function"){
-				options.renderProductsFallBack();
+			if(typeof options.renderProductsCallBack == "function"){
+				options.renderProductsCallBack();
 			}
 		}
-
-	},
-	renderTemplate:function(products,template,target,currency){
-		if(template.length == 0 && target.length == 0){
+		else{
+			self.consoleLog('error','Please revise the products JSON object');
+		}
+	}
+	this.renderTemplate = function(products,template,target,currency){
+		if(template.length == 0 || target.length == 0){
 			self.consoleLog('error','There is No Template/Listing');
 			return;
 		}
 		Mustache.parse(template);   // optional, speeds up future uses
-		var rendered = Mustache.render(template, {products : products,currency:currency});
-		target.innerHTML = rendered;
-	},
-	updateCart:function(id,type){
-		var self = this;
-		var options = self.options;
-		if(typeof self.JsonCart == "undefined"){
-			self.JsonCart = new Array;
-		}
-		if(self.getProductbyProperty(self.JsonCart,'id',id)){
-			var cartProduct = self.getProductbyProperty(self.JsonCart,'id',id);
-			if(type == "add"){
-				cartProduct.qty++;
-				options.addProductFallBack();
-			} 
-			else{
-				cartProduct.qty--;
-				self.deleteObjectId(id,self.JsonCart);
-				options.removeProductFallBack();
-			}
+		target.innerHTML = Mustache.render(template, {products : products,currency:currency});
+	}
+	this.updateCart = function(id,type){
+		var cartProduct = self.getObjectbyProperty(JsonCart,'id',id);
+		if(cartProduct){
+			cartActions[type](cartProduct);
 		}else{
-			product = self.getProductbyProperty(options.products,'id',id);
+			product = self.getObjectbyProperty(options.products,'id',id);
 			product.qty = 1;
-			self.JsonCart.push(product);
-			options.addProductFallBack();
+			JsonCart.push(product);
+			if(typeof options.addProductCallBack == "function"){
+				options.addProductCallBack();
+			}
+
 		}
-		var cartTemplate = options.cartHtmlTemplate;
-		var cartHtmlTarget = options.cartHtmlObject;
-		var currency = options.currency;
-		self.JsonCart['total'] = self.getCartTotal(self.JsonCart);
+		JsonCart['total'] = self.getCartTotal(JsonCart);
 		if(options.saveCart){
-			self.pushLocalStorageCart(self.JsonCart);
+			self.pushLocalStorageCart(JsonCart);
 		}
-		self.renderTemplate(self.JsonCart,cartTemplate,cartHtmlTarget,currency);
-	},
-	getCartTotal:function(cart){
+		self.renderTemplate(JsonCart,options.cartHtmlTemplate,options.cartHtmlObject,options.currency);
+	}
+	this.getCartTotal = function(cart){
 		var total = 0;
 		for (var i = cart.length - 1; i >= 0; i--) {
-			currentProduct = cart[i];
-			total += currentProduct.price * currentProduct.qty;
+			total += cart[i].price * cart[i].qty;
 		};
 		return total;
-	},
-	validateProductsJson:function(products){
-		var self = this;
+	}
+	this.validateProductsJson = function(products){
 		var validateThese = ['name','id','price','image'];
-		if(products.length > 0){
-			for(var i in products){
-				for (var x = 0; x < validateThese.length; x++) {
-					if(!products[i].hasOwnProperty(validateThese[x])){
-						self.consoleLog('warn',"product #"+ i +" doesn't have "+validateThese[x]);
-						return false;
-					}
-				}
-				i++;
-			}
-			return true;
-		}else{
+		if(products.length ===  0){
 			return false;
 		}
-	},
-	pushLocalStorageCart:function(cart){
-		if(typeof(Storage)!=="undefined"){
-			var cartString = JSON.stringify(cart);
-			localStorage.setItem("TempCart", cartString);
-		  }	
-	},
-	getLocalStorageCart:function(){
-		var self = this;
-		var options = self.options;
-		if(typeof(Storage)!=="undefined"){
-			var cartString = localStorage.getItem('TempCart');
-			self.JsonCart = JSON.parse(cartString);
-			var cartTemplate = options.cartHtmlTemplate;
-			var cartHtmlTarget = options.cartHtmlObject;
-			var currency = options.currency;
-			self.JsonCart['total'] = self.getCartTotal(self.JsonCart);
-			self.renderTemplate(self.JsonCart,cartTemplate,cartHtmlTarget,currency);
-		  }	
-	},
-	checkLocalStorageCart:function(){
-		return typeof(Storage) !=="undefined" && localStorage.getItem('TempCart').length > 0;	
-	},
-	consoleLog:function(type,message){
-		if(self.consoleReady == true){
+		for (var i = products.length - 1; i >= 0; i--) {
+			for (var m = 0; m < validateThese.length; m++) {
+				if( products[i][validateThese[m]] == null || typeof products[i][validateThese[m]] == 'undefined'){
+					self.consoleLog('warn',"product #"+ i +" doesn't have "+validateThese[m]);
+					return false; 
+				}
+			};
+		};
+		return true;
+	}
+	this.pushLocalStorageCart = function(cart){
+		if(typeof(Storage) ==="undefined"){
+			return;
+		}
+		var cartString = JSON.stringify(cart);
+		localStorage.setItem("TempCart", cartString);
+	}
+	this.getLocalStorageCart = function(){
+		if(typeof(Storage) ==="undefined"){
+			return;
+		}	
+		var cartString = localStorage.getItem('TempCart');
+		JsonCart = JSON.parse(cartString);
+		JsonCart['total'] = self.getCartTotal(JsonCart);
+		self.renderTemplate(JsonCart,options.cartHtmlTemplate,options.cartHtmlObject,options.currency);
+
+	}
+	this.checkLocalStorageCart = function(){
+		return typeof(Storage) !=="undefined" && localStorage.getItem('TempCart') != null ;	
+	}
+	this.consoleLog = function(type,message){
+		if(consoleReady){
 			console[type]('Temp Cart : ' + message);
 		}
 		else{
 			alert(message);
 		}
-	},
-	getProductbyProperty:function(obj,prop,value){//helper
+	}
+	this.getObjectbyProperty = function(obj,prop,value){//helper
 		for (var i = obj.length - 1; i >= 0; i--) {
 			if(obj[i][prop] == value){
 				return obj[i];
 			}
 		}
-	},
-	deleteObjectId:function(id,obj){
+	}
+	this.deleteObjectId = function(id,obj){
 		for (var i = obj.length - 1; i >= 0; i--) {
 			if(obj[i]['id'] == id){
 				obj.splice(i,1);
 			}
 		}
 	}
-}
+};
